5ea0143b52be927e62d24d4b3805c30bb7866635
palomatanis <paloma.pedregal@gmail.com>
2016-11-15 02:23:35 +0100

    base pero errores sin corregir

--- init.el
+++ init.el
@@ -3,48 +3,49 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
+(package-initialize nil)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
+(add-to-list 'load-path "~/.emacs.d/plugins")
 (add-to-list 'load-path "~/.emacs.d/settings/")
 
-(require 'theme)
+(require 'theme-settings)
+
+;;(set-linum)
+;;(set-full-paren)
+;;(set-sanityinc)
+
 (require 'package-settings)
-;;(require 'helm-settings)
-;;(require 'org-settings)
-(global-linum-mode 1)
 
-(if (file-exists-p "/home/paloma/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
-(load-file "/home/paloma/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
-)
+;;(require 'general-settings)
+
+;; ;;(require 'helm-settings)
+;; (global-linum-mode 1)
+
+;; ;;(require 'term-settings)
+;; ;;(set-multi-term)
+;; ;;;; (set-emux)
+;; ;;;; (emux-default-sessions)
+
+;; ;;(require 'org-settings)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages (quote (elm-mode scala-mode multi-term helm matlab-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-(defun iwb ()
-"Indent whole buffer"
-(interactive)
-(delete-trailing-whitespace)
-(indent-region (point-min) (point-max) nil)
-(untabify (point-min) (point-max))
-(message "Indent buffer: Done.")
-)
+;; (require 'random-requires) ;; Functions for some modes and other things
+;; (set-undo) ;; Ctrl-z powered
+;; (set-autoindent) ;; Thanks for indent!
+;; (set-auto-complete) ;; Auto complete with local definitions
+;; (set-scroll) ;; Smooth scroll ^^
+;; (set-indent-guide)  ;; The indent vertical lines.. cute!
+;; (set-pair)  ;; How to pair? autopair or smartparens?
+;; (set-multiple-cursors)  ;; The more, the better
+;; (set-uniquify)  ;; Buffer names uniques! (set-auto-package) ;; Auto update all
 
-(global-set-key (kbd "M-i") 'iwb)
+
+(require 'programming-settings)
+
+
+
+;;; init.el ends here
+
+
 
---
+++ settings/prolog-settings.el
@@ -0,0 +1,8 @@
+
+(if (file-exists-p "/home/paloma/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
+(load-file "/home/paloma/CiaoDE-1.15-1781-g328b907/emacs-mode/ciao-site-file.el")
+)
+
+
+
+(provide 'prolog-settings)
